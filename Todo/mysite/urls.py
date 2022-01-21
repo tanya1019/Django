c323be7f3a6dd81535db17943213713806a4f0e7
Tanya Thakur <tanya.vanneri19@gmail.com>
2022-01-21 19:12:06 +0530

    'deployment'

--- Todo/mysite/urls.py
+++ Todo/mysite/urls.py
@@ -16,12 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp import views
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name = 'index'),
-    path('delete/<int:taskid>/', views.delete, name = 'delete'),
+    path('', views.index, name='index'),
+    path('delete/<int:taskid>/', views.delete, name='delete'),
     path('update/<int:id>/', views.update, name='update'),
-    path('cbvindex/', views.TaskListView.as_view(), name = 'cbvindex'),
-    path('cbvdetail/<int:pk>/', views.TaskDetailView.as_view(), name = 'cbvdetail'),
-    path('cbvupdate/<int:pk>/', views.TaskUpdateView.as_view(), name = 'cbvupdate'),
+    path('cbvindex/', views.TaskListView.as_view(), name='cbvindex'),
+    path('cbvdetail/<int:pk>/', views.TaskDetailView.as_view(), name='cbvdetail'),
+    path('cbvupdate/<int:pk>/', views.TaskUpdateView.as_view(), name='cbvupdate'),
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ]
