90393883ea8ab0056aa2b30c368eaa01c627e833
Tanya Thakur <tanya.vanneri19@gmail.com>
2022-01-21 22:14:27 +0530

    delete

--- Todo/mysite/settings.py
+++ Todo/mysite/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k8)z0icj5q#0+cprj+bfsk@%^&tpc6%#qexf$f+(gk-or7xn+s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'dailyplans.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- Todo/mysite/urls.py
+++ Todo/mysite/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp import views
-from django.views.static import serve
-from django.conf.urls import url
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
@@ -27,8 +24,4 @@ urlpatterns = [
     path('cbvindex/', views.TaskListView.as_view(), name='cbvindex'),
     path('cbvdetail/<int:pk>/', views.TaskDetailView.as_view(), name='cbvdetail'),
     path('cbvupdate/<int:pk>/', views.TaskUpdateView.as_view(), name='cbvupdate'),
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
 ]
